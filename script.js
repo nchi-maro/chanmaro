d5b5378fc938aca4c0af344660a741629933b0fc
nchi-maro <145962625+nchi-maro@users.noreply.github.com>
2023-09-25 15:45:30 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+console.log('Hello World!');
+const target = document.getElementById('target');
+//target.classList.add('red');
+target.addEventListener('click', () => {
+        target.classList.add('bule');
+    });
+
